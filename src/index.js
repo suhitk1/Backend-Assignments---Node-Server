ae315b6f068e5e7dd7510cbcec36f5dffa35386b
suhitk1 <suhitk2112@gmail.com>
2020-11-27 20:02:53 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -6,19 +6,16 @@ const httpServer = http.createServer(handleServer);
 function handleServer(req, res) {
     if(req.url.path === '/welcome'){
         res.writeHead(200, {'Content-Type': 'text/plain'});
-        //res.status(200);
         res.write('Welcome to Dominos!');
         res.end();
     }else if(req.url.path === '/contact'){
         res.writeHead(200, {'Content-Type': 'application/json'});
-        res.status(200).json({
+        res.end(JSON.stringify({
             phone: '18602100000',
             email: 'guestcaredominos@jublfood.com'
-            });
-        //res.write('Welcome to Dominos!');
-        res.end();
+            }));
     }else{
-        res.status(404);
+        res.writeHead(404, {});
         res.end();
     }
 }
